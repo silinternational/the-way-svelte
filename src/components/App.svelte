6562ce098e7362d8824c070900e0c47f0ff14459
Billy Clark <billy_clark@sil.org>
2020-06-02 21:42:21 -0400

    housekeeping

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,7 +1,7 @@
 <script>
+import { parse, stringify } from 'qs'
 import { Router } from "@sveltech/routify"
 import { routes } from "@sveltech/routify/tmp/routes"
-import { parse, stringify } from 'qs'
 import t from '../i18n'
 
 // added because of this:  https://github.com/sveltech/routify/issues/201
