4bc468f0aba162379816f23d90912877d0341706
Billy Clark <billy_clark@sil.org>
2020-06-24 13:25:47 -0400

    bug: i18n had no fallback

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,6 +1,12 @@
 import phrases from './phrases.json'
 
 export default function t(key) {
+  const phrase = phrases[key]
+  if (phrase === undefined) {
+    return '⤂ translation key not found! ⤃'
+  }
+
   const langOnlyNoVariant = navigator.language.substring(0,2)
-  return phrases[key][langOnlyNoVariant || 'en']
+
+  return phrase[langOnlyNoVariant] || phrase['en']
 }
